d4dab23cf1c15906fbab6d16fb7eef2278c330c3
Gabriel Ferreira <gabriel.ferreira@concretesolutions.com.br>
2016-05-18 19:49:19 -0300

    lesson 3

---
+++ lesson3/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World</h1>
+                <b>Bold</b>
+            </div>
+        );
+    }
+}
+
+// const App = () => <h1>Hello World</h1>
+
+export default App
